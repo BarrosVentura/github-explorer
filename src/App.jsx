01558bbf5b0b6e834eaa6aeded0ac29b5d47c8cf
Adson Ventura <adsonventura@gmail.com>
2022-05-14 16:26:15 -0300

    creates new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss";
 
 export function App() {
-  return <h1>Hello Worlde</h1>;
+  return <RepositoryList />;
 }
